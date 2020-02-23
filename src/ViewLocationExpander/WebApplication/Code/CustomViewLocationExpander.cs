b7ac909f41d61db6c51f5fd04951654028b3f930
fernandezja <fernandezja@gmail.com>
2020-02-23 17:33:16 -0300

    Sample ViewLocationExpander (add many folder to search views and components to razor view engine)

---
+++ src/ViewLocationExpander/WebApplication/Code/CustomViewLocationExpander.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Code
+{
+    public class CustomViewLocationExpander : IViewLocationExpander
+    {
+        public static IEnumerable<string> Locations
+        {
+            get
+            {
+                return new string[] {"/Views/{1}/{0}.cshtml",
+                                    "/Views/Shared/{0}.cshtml",
+                                    "/Views/Components/{1}/{0}.cshtml",
+
+                                    "/ViewExternalFolder2/{0}.cshtml",
+                                    "/ViewExternalFolder2/{1}/{0}.cshtml",
+                                    "/ViewExternalFolder2/Shared/{0}.cshtml",
+                                    "/ViewExternalFolder2/Components/{0}.cshtml",
+
+                                     "/ViewExternalFolder3/{0}.cshtml"
+                                    };
+            }
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            return Locations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            context.Values["customviewlocation"] = nameof(CustomViewLocationExpander);
+        }
+    }
+}
