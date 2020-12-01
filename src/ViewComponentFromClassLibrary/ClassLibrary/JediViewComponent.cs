83e0c6c346b4b01a9e2d767b59b8cb1c5de90e4c
fernandezja <fernandezja@gmail.com>
2020-12-01 11:41:55 -0300

    View component from class library

---
+++ src/ViewComponentFromClassLibrary/ClassLibrary/JediViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class JediViewComponent: ViewComponent
+    {
+
+        public JediViewComponent()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string jediName)
+        {
+            var view = "JediCustomView";
+
+            return View(view, jediName);
+        }
+
+
+    }
+}
