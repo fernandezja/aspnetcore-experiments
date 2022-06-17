6b141909c1d50b3fbebb169c5c4f519e62e87ec0
fernandezja <fernandezja@gmail.com>
2022-06-17 12:46:00 -0300

    [ApplicationPart] Shared controllers and view from library

---
+++ src/ApplicationPart/SharedComponents/Controllers/JediController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharedComponents.Controllers
+{
+    public class JediController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/ApplicationPart/WebApplication1/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+//
+var assembly = typeof(SharedComponents.Controllers.JediController).Assembly;
+builder.Services.AddControllersWithViews()
+                .AddApplicationPart(assembly)
+                .AddRazorRuntimeCompilation();
+
+builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(
+    options => { 
+        options.FileProviders.Add(new EmbeddedFileProvider(assembly)); 
+    });
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
