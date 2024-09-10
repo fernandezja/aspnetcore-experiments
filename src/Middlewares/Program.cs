7ad81917326f2d6ecfa8d267a86f7be4894b43d3
Jose A. Fernandez <fernandezja@gmail.com>
2024-09-10 20:49:33 -0300

    [Middleware] Map, MapWhen, UseWhen, UseStaticFiles

--- src/Middlewares/Program.cs
+++ src/Middlewares/Program.cs
@@ -5,7 +5,7 @@ var app = builder.Build();
 
 //app.UseResponseCaching();
 //app.UseResponseCompression();
-//app.UseStaticFiles();
+app.UseStaticFiles();
 
 app.Use(async (context, next) =>
 {
@@ -18,8 +18,17 @@ app.Use(async (context, next) =>
 {
     Console.WriteLine("Middleware 2.Start");
     await next.Invoke();
-    Console.WriteLine("Middleware 2.Endt");
+    Console.WriteLine("Middleware 2.End");
 });
+
+app.Map("/maptest1", HandleMapTest1);
+
+app.MapWhen(context => context.Request.Query.ContainsKey("demo1"), 
+                      HandleDemo1Request);
+
+app.UseWhen(context => context.Request.Query.ContainsKey("demo2"),
+            appBuilder => HandleDemo2Request(appBuilder));
+
 
 
 app.Run(async context =>
@@ -28,3 +37,38 @@ app.Run(async context =>
 });
 
 app.Run();
+
+
+
+static void HandleMapTest1(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Map Test 1");
+    });
+}
+
+
+static void HandleDemo1Request(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        var demoValue = context.Request.Query["demo1"];
+        await context.Response.WriteAsync($"Demo1 value = {demoValue}");
+    });
+}
+
+void HandleDemo2Request(IApplicationBuilder app)
+{
+    var logger = app.ApplicationServices.GetRequiredService<ILogger<Program>>();
+
+    app.Use(async (context, next) =>
+    {
+        var demoValue = context.Request.Query["demo2"];
+        logger.LogInformation("Demo2 value = {demo2}", demoValue);
+
+        // Do work that doesn't write to the Response.
+        await next();
+        // Do other work that doesn't write to the Response.
+    });
+}
